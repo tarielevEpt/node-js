93a5c38faad6bf1a406df52efe3dd0435936cd82
Uluk <tarielevuluk@gmail.com>
2023-04-18 11:10:25 +0600

    massage in node js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var fs=require("fs")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get("/toTxt", function(reg,res){
+  fs.appendFileSync("car.txt", reg.query.autoName+" "+reg.query.year+' '+reg.query.price+',')
+  res.render("car.hbs", {})
+})
+
+router.get("/listOfCars", function(reg,res){
+  let carsList=fs.readFileSync("car.txt", "utf8")
+  res.render("carsList.hbs", {
+    key1: carsList
+  })
+})
 
 module.exports = router;
